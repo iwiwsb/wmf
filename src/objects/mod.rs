39c34e6a7261e04da63267f5de37698a4181a861
iwiwsb <30194859+iwiwsb@users.noreply.github.com>
2023-09-26 12:19:09 +0300

    Move objects to module

---
+++ src/objects/mod.rs
@@ -0,0 +1,53 @@
+pub struct Brush {}
+
+pub struct Font {}
+
+pub struct Palette {}
+
+pub struct Pen {}
+
+pub struct Region {}
+
+pub struct Bitmap16 {}
+
+pub struct BitmapCoreHeader {}
+
+pub struct BitmapInfoHeader {}
+
+pub struct BitmapV4Header {}
+
+pub struct BitmapV5Header {}
+
+pub struct CieXyz {}
+
+pub struct CieXyzTriple {}
+
+pub struct ColorRef {}
+
+pub struct DeviceIndependentBitmap {}
+
+pub struct LogBrush {}
+
+pub struct LogColorSpace {}
+
+pub struct LogColorSpaceW {}
+
+pub struct PaletteEntry {}
+
+pub struct PitchAndFamily {}
+
+pub struct PointL {}
+
+pub struct PointS {}
+
+pub struct PolyPolygon {}
+
+pub struct Rect {}
+
+pub struct RectL {}
+
+pub struct RGBQuad {}
+
+pub struct Scan {}
+
+pub struct SizeL {}
